44505a08461284debf15e8c451644f2756a0c5cd
karthik18618 <97032695+karthik18618@users.noreply.github.com>
2022-01-04 07:53:32 +0530

    Add files via upload

---
+++ ArrayList1/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+    ArrayList l1= new ArrayList();
+            l1.add(12);
+            l1.add(14);
+            l1.add(16);
+            System.out.println(l1);
+
+    }
+}
